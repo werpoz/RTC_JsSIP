7c249bbae840bc9a25269597e8252256dbc23c07
Abel Jean Pool Seminario Leon <aseminario@anikamagroup.com>
2021-07-14 00:22:58 -0500

    first commit

---
+++ jsSIP/index.js
@@ -0,0 +1,35 @@
+// Create our JsSIP instance and run it:
+var JsSIP = require('jssip');
+var socket = new JsSIP.WebSocketInterface('wss://sip.myhost.com');
+var configuration = {
+  sockets  : [ socket ],
+  uri      : 'sip:alice@example.com',
+  password : 'superpassword'
+};
+
+var ua = new JsSIP.UA(configuration);
+
+ua.start();
+
+// Register callbacks to desired call events
+var eventHandlers = {
+  'progress': function(e) {
+    console.log('call is in progress');
+  },
+  'failed': function(e) {
+    console.log('call failed with cause: '+ e.data.cause);
+  },
+  'ended': function(e) {
+    console.log('call ended with cause: '+ e.data.cause);
+  },
+  'confirmed': function(e) {
+    console.log('call confirmed');
+  }
+};
+
+var options = {
+  'eventHandlers'    : eventHandlers,
+  'mediaConstraints' : { 'audio': true, 'video': true }
+};
+
+var session = ua.call('sip:bob@example.com', options);
